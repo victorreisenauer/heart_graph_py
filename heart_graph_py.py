c8feec6dd72a0c7b9b2152c4b14e9bf16ea49c1e
Victor Reisenauer <victorreisenauer99@gmail.com>
2019-01-16 21:34:18 +0100

    feat: Create basic graph with simple values

---
+++ heart_graph_py.py
@@ -0,0 +1,36 @@
+"""main script for the heart_graph_py.py project. Read README.md for more information"""
+
+# imports
+from pylab import *
+import numpy as np
+
+def cube(x):
+    """ 
+    helper function to calculate the cube of a 
+    negative number (complex numbers)
+    """
+    if 0 <= x: 
+        return x**(1./3.)
+    return -(-x)**(1./3.)
+
+def calculate_datapoints(a):
+    """
+    calculate an array of datapoints with this special function:
+    y = x**(2/3) + 0.9*(3.3 - x**2)**(1/2) * sin(a*pi*x)
+    """
+    x = np.arange(-2.0, 2.0, 0.01)
+    x = np.around(x, decimals=3)
+    y = x
+    data = [x, y]
+    return data
+
+def plot_graph(x, y):
+    plot(x, y)
+    grid(True)
+    show()
+
+if __name__ == '__main__':
+	"""don't excecute when script is used as a module"""
+	data = calculate_datapoints(10)
+	plot_graph(data[0], data[1])
+
