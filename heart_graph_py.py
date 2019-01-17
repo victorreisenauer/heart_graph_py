6ad3b5f523c7275b9317ddce8bd05196a0589381
Victor Reisenauer <victorreisenauer99@gmail.com>
2019-01-17 19:28:34 +0100

    feat: Add the title of the graph

--- heart_graph_py.py
+++ heart_graph_py.py
@@ -3,7 +3,6 @@
 # imports
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib as mpl
 from pylab import *
 from time import sleep
 from random import randint
@@ -14,7 +13,7 @@ def cube(x):
     helper function to calculate the cube of a
     negative number (complex numbers)
     """
-    if 0 <= x: 
+    if 0 <= x:
         return x**(1./3.)
     return -(-x)**(1./3.)
 
@@ -34,18 +33,21 @@ def plot_graph(x, y):
     Generate and customize a graph.
     Also plot the values passed from calculate_datapoints().
     """
-    plt.ion()
 
+    # get up some basic graph parameters
+    plt.ion()
     fig = plt.figure()
     ax = fig.add_subplot(111)
+    ax.set_title("x^(2/3) + 0.9*(3.3 - x^2)^(1/2) * sin(a * pie * x)")
     gca().set_position((.1, .3, .8, .6))
     line1, = ax.plot(x, y, 'b-', linewidth=2.5)
     grid(True)
     plt.xlim(-4, 4)
     plt.ylim(-2, 3.5)
+
+    # add two texts to randomly appear under the graph
     randomizer = randint(0, 10)
     if randomizer < 5:
-
         figtext(.02, .02,
                 """
          \"Liebe muss nicht bitten, auch nicht fordern.
@@ -60,15 +62,17 @@ def plot_graph(x, y):
             Die meisten Menschen aber rechnen, um sich zu verlieren.\"
                             - Hermann Hesse (gewisse Änderungen von von Victor Reisenauer)
                 """)
-    text_1 = ax.text(x=2, y=2, s='' )
+    text_1 = ax.text(x=2, y=2, s='')
+
+    # add an increasing phase (parameter a in the function) to change the function over time
     for phase in range(0, 1500, 15):
         speed = 100
         text_1.set_text('parameter a = ' + str(phase/100))
         line1.set_ydata(calculate_datapoints(phase/speed)[1])
         fig.canvas.draw()
         sleep(0.05)
-    text_1.set_text('Damn this is cheesy :P')
-    plt.show(block=True)
+    text_1.set_text('Damn this is cheesy :P')		# text appears when param_a is at it's max
+    plt.show(block=True)							# stop window from closing immediately
 
 
 if __name__ == '__main__':
